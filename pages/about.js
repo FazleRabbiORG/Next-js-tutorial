50abb4ab85d72b0976be6d0ec72694c3d9b732e4
Fazle Rabbi <24.fazlerabbi@gmail.com>
2022-03-30 00:45:11 +0600

    lecture 4

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+function about() {
+  return (
+    <div>
+      <h1>this is about page</h1>
+    </div>
+  );
+}
+
+export default about;
---
+++ pages/profile.js
@@ -0,0 +1,9 @@
+function profile() {
+  return (
+    <div>
+      <h2>this is profile page</h2>
+    </div>
+  );
+}
+
+export default profile;

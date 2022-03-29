9f616efbce4b527ffceeb05042f8cc9e294a9551
Fazle Rabbi <24.fazlerabbi@gmail.com>
2022-03-30 01:07:26 +0600

    lecture 6

--- pages/blog/howtobeamaterofnextjs.js
+++
@@ -1,5 +0,0 @@
-function howtobeamaterofnextjs() {
-  return <div>How to be a master of next js</div>;
-}
-
-export default howtobeamaterofnextjs;
---
+++ pages/blog/index.js
@@ -0,0 +1,12 @@
+function index() {
+  return (
+    <div>
+      <h1>blog 1</h1>
+      <h1>blog 2</h1>
+      <h1>blog 3</h1>
+      <h1>blog 4</h1>
+    </div>
+  );
+}
+
+export default index;
